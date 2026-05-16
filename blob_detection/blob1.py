69aee2d42a2c9af60e12d1382ea83345b0c43959
Michael Wall <walmic02@icloud.com>
2026-05-15 20:17:51 -0400

    added deque jitter detection, removed excess filtering

--- blob_detection/blob1.py
+++ blob_detection/blob1.py
@@ -4,6 +4,7 @@ os.environ["OPENCV_VIDEOIO_MSMF_ENABLE_HW_TRANSFORMS"] = "0"
 import numpy as np
 import cv2 as cv
 import undistort as ud
+from collections import deque
 
 def nothing(x):
     pass
@@ -11,19 +12,21 @@ def nothing(x):
 cap = cv.VideoCapture(0)
 cap.set(cv.CAP_PROP_FPS, 60.0) 
 cap.set(cv.CAP_PROP_FRAME_WIDTH,1920)
-cap.set(cv.CAP_PROP_FRAME_HEIGHT,1080) 
+cap.set(cv.CAP_PROP_FRAME_HEIGHT,1080)
 
 cv.namedWindow('display', cv.WINDOW_NORMAL)
-cv.resizeWindow('display', 640,360)
+cv.resizeWindow('display', 640,400)
 
 cv.createTrackbar('hue_lower', 'display', 5, 179, nothing)
 cv.createTrackbar('hue_upper', 'display', 20, 179, nothing)
-# cv.createTrackbar('minRadius', 'display', 75, 200, nothing)
-# cv.createTrackbar('maxRadius', 'display', 120, 200, nothing)
-param1 = 100
-param2 = 15
-minRadius = 75
-maxRadius = 120
+# cv.createTrackbar('min_sat', 'display', 80, 255, nothing)
+# cv.createTrackbar('min_value', 'display', 80, 255, nothing)
+
+# store recent positions
+history_x = deque(maxlen=300)
+history_y = deque(maxlen=300)
+
+crosshair_length = 100
 
 kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (11,11))
 
@@ -44,26 +47,41 @@ while True:
     
     lower_hue = cv.getTrackbarPos('hue_lower', 'display')
     upper_hue = cv.getTrackbarPos('hue_upper', 'display')
+    # min_sat = cv.getTrackbarPos('min_sat', 'display')
+    # min_value = cv.getTrackbarPos('min_value', 'display')
+
 
     # define range of blue color in HSV
-    lower = np.array([lower_hue,80,80])
+    lower = np.array([lower_hue,140,140])
     upper = np.array([upper_hue,255,255])
 
     blur_frame = cv.GaussianBlur(frame, (9,9), 0)
     hsv = cv.cvtColor(blur_frame, cv.COLOR_BGR2HSV)
     mask = cv.inRange(hsv, lower, upper)
-    opening = cv.morphologyEx(mask, cv.MORPH_OPEN, kernel)
-    edge_blur = cv.GaussianBlur(opening, (5,5), 0)
-    _, thresh = cv.threshold(edge_blur, 127, 255, cv.THRESH_BINARY)
+    # opening = cv.morphologyEx(mask, cv.MORPH_OPEN, kernel)
 
-    M = cv.moments(thresh)
+    M = cv.moments(mask)
     if M["m00"] > 0:
-        cx_m = int(M["m10"] / M["m00"])
-        cy_m = int(M["m01"] / M["m00"])
-        cv.circle(frame, (cx_m, cy_m), 5, (0, 0, 255), -1)      #center dot
+        cx = M["m10"] / M["m00"]
+        cy = M["m01"] / M["m00"]
+        history_x.append(cx)
+        history_y.append(cy)
 
-    cv.imshow('display', opening)
+        cv.putText(frame, f"X: {cx:.02f} Y: {cy:.02f}", (int(cx) + 10, int(cy) - 10),cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+        cx = round(cx)
+        cy = round(cy)
+        cv.circle(frame, (cx, cy), 8, (0, 0, 255), -1)      #center dot
+        cv.line(frame,(cx-crosshair_length, cy),(cx+crosshair_length, cy),(0,0,0),2)
+        cv.line(frame,(cx, cy-crosshair_length),(cx, cy+crosshair_length),(0,0,0),2)
+
+    cv.imshow('display', mask)
     cv.imshow('display2', frame)
+    if len(history_x) > 30:
+        x_std = np.std(history_x)
+        y_std = np.std(history_y)
+
+        print(f"Jitter X: ±{x_std:.4f} px | "
+                f"Jitter Y: ±{y_std:.4f} px")
 
     if cv.waitKey(1) == 27:
         break
