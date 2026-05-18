03b3c44edcae93605e1fc89d9fccefc4403c78ee
Michael Wall <walmic02@icloud.com>
2026-05-17 21:49:20 -0400

    distance calc with yellow markers on beam

--- blob_detection/blob1.py
+++ blob_detection/blob1.py
@@ -17,25 +17,38 @@ def find_moment(mask):
         cx = cy = None
     return cx, cy
 
+def calc_proj(A, B, P):
+    AB = B - A
+    AP = P - A
+    t = np.dot(AP, AB) / np.dot(AB, AB)
+    return (A + t * AB)
+
+
 cap = cv.VideoCapture(0)
 cap.set(cv.CAP_PROP_FPS, 60.0) 
 cap.set(cv.CAP_PROP_FRAME_WIDTH,1920)
 cap.set(cv.CAP_PROP_FRAME_HEIGHT,1080)
 
-cv.namedWindow('display', cv.WINDOW_NORMAL)
-cv.resizeWindow('display', 640,400)
+cv.namedWindow('mask', cv.WINDOW_NORMAL)
+cv.resizeWindow('mask', 640,400)
 
-cv.createTrackbar('hue_lower', 'display', 80, 179, nothing)
-cv.createTrackbar('hue_upper', 'display', 115, 179, nothing)
-cv.createTrackbar('min_sat', 'display', 140, 255, nothing)
-cv.createTrackbar('min_value', 'display', 64, 255, nothing)
+cv.createTrackbar('hue_lower', 'mask', 25, 179, nothing)
+cv.createTrackbar('hue_upper', 'mask', 35, 179, nothing)
+cv.createTrackbar('min_sat', 'mask', 60, 255, nothing)
+cv.createTrackbar('min_value', 'mask', 95, 255, nothing)
 
-lower_ball = np.array([5,180,180])
+lower_ball = np.array([5,140,140])
 upper_ball = np.array([20,255,255])
+
+
+# lower_beam = np.array([25,60,95])
+# upper_beam = np.array([35,255,255])
 
 crosshair_length = 100
 
 k = cv.KalmanFilter(4, 2)
+
+dist = 0
 
 if not cap.isOpened():
     print("Cannot open camera")
@@ -46,6 +59,15 @@ while True:
     if not ret:
         print("Can't receive frame")
         break
+    
+    lower_hue = cv.getTrackbarPos('hue_lower', 'mask')
+    upper_hue = cv.getTrackbarPos('hue_upper', 'mask')
+    min_sat = cv.getTrackbarPos('min_sat', 'mask')
+    min_value = cv.getTrackbarPos('min_value', 'mask')
+
+    # define range of blue color in HSV
+    lower_beam = np.array([lower_hue,min_sat,min_value])
+    upper_beam = np.array([upper_hue,255,255])
 
     frame = ud.undistort(frame)
 
@@ -62,14 +84,6 @@ while True:
         cv.line(frame,(cx-crosshair_length, cy),(cx+crosshair_length, cy),(0,0,0),2)
         cv.line(frame,(cx, cy-crosshair_length),(cx, cy+crosshair_length),(0,0,0),2)
 
-    lower_hue = cv.getTrackbarPos('hue_lower', 'display')
-    upper_hue = cv.getTrackbarPos('hue_upper', 'display')
-    min_sat = cv.getTrackbarPos('min_sat', 'display')
-    min_value = cv.getTrackbarPos('min_value', 'display')
-
-    # define range of blue color in HSV
-    lower_beam = np.array([lower_hue,min_sat,min_value])
-    upper_beam = np.array([upper_hue,255,255])
     beam_mask = cv.inRange(hsv, lower_beam, upper_beam)
 
     if beam_mask is not None:
@@ -88,8 +102,20 @@ while True:
         if xb0 is not None and xb1 is not None:
             cv.line(frame, (xb0, yb0), (xb1, yb1), (255, 0, 0), 2)
 
-    cv.imshow('display', beam_mask)
-    cv.imshow('display2', frame)
+            A = np.array([xb0, yb0], dtype=np.float32)
+            B = np.array([xb1, yb1], dtype=np.float32)
+            P = np.array([cx, cy], dtype=np.float32)
+            Q = calc_proj(A, B, P)
+            dist = np.linalg.norm(Q - A)
+            print(dist)
+
+            cv.circle(frame, tuple(Q.astype(int)), 8, (0, 0, 255), -1)
+            cv.line(frame, tuple(P.astype(int)),tuple(Q.astype(int)), (0, 255, 0), 2)
+
+
+
+    cv.imshow('display', frame)
+    cv.imshow('mask', beam_mask)
 
     if cv.waitKey(1) == 27:
         break
